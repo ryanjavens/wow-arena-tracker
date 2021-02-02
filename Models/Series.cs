ed575994754c9855785e4c1715217511f0e29aa9
Ryan Javens <raykon1499@gmail.com>
2021-02-01 20:12:18 -0500

    Created a number of initial data models

---
+++ Models/Series.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WowArenaTracker.Models {
+    public class Series {
+        public int ID { get; set; }
+        
+        // Foreign keys
+        public Team[] Teams { get; set; }
+        public int WinningTeamScore { get; set; }
+        public int LosingTeamScore { get; set; }
+        public Game[] Games { get; set; }
+    }
+}
---
+++ Models/Tournament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WowArenaTracker.Models {
+    public class Tournament {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        // Foreign keys
+        public Series[] SeriesList { get; set; }
+        // TODO: WinningTeam may be unneeded...
+        public Team WinningTeam { get; set; }
+    }
+}
