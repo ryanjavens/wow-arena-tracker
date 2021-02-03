812cdbbd6bb9324874a8a15253758131f0b9cba5
DESKTOP-10HH88I\steve <steve.klotz13@gmail.com>
2021-02-02 20:16:55 -0500

    Project startup settings, Program settings, app settings, and launch settings.  I added services for http context client for making http requests to foreign api's, logging service, and swagger for automatic documentation of services, data models, and controllers.

--- Program.cs
+++ Program.cs
@@ -2,20 +2,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NLog.Extensions.Logging;
 
-namespace WowArenaTracker {
-    public class Program {
-        public static void Main(string[] args) {
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
+                .ConfigureLogging((hostingcontext, logging) =>
+                {
+                    //Configure logging for the application
+                    logging.AddConfiguration(hostingcontext.Configuration.GetSection("Logging"));
+                    logging.AddDebug();
+                    logging.AddNLog();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
                     webBuilder.UseStartup<Startup>();
                 });
     }
